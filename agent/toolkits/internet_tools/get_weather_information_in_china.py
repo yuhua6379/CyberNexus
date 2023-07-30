4813bb3c1533a1db75a827535a7bf602bb387ac5
yuhua6379 <yuhua6379@gmail.com>
2023-07-30 10:54:02 +0800

    feat: 用环境变量传key

--- agent/toolkits/internet_tools/get_weather_information_in_china.py
+++ agent/toolkits/internet_tools/get_weather_information_in_china.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from langchain.tools import BaseTool
 from pydantic import BaseModel
 import requests
@@ -24,25 +26,29 @@ class GetWeatherInformationInChina(BaseTool):
                    "secondly, you must make sure the location is a valid city.")
 
     @staticmethod
-    def get_weather(location: str) -> WeatherInfo:
-        logging.info(f"location of the request is {location}")
-        resp = requests.get(
-            f'http://www.tianqiapi.com/api?version=v6&appid=23035354&appsecret=8YvlPNrz&city={location}')
-        resp.encoding = 'utf-8'
-        content = resp.json()
+    def get_weather(location: str) -> Union[WeatherInfo, str]:
+        try:
+            logging.info(f"location of the request is {location}")
+            resp = requests.get(
+                f'http://www.tianqiapi.com/api?version=v6&appid=23035354&appsecret=8YvlPNrz&city={location}')
+            resp.encoding = 'utf-8'
+            content = resp.json()
 
-        wi = WeatherInfo(
-            city=content['city'],
-            country=content['country'],
-            wind=content['win'],
-            wind_speed=content['win_speed'],
-            air_level=content['air_level'],
-            temperature_now=content['tem'],
-            temperature_lowest=content['tem2'],
-            temperature_highest=content['tem1'])
+            wi = WeatherInfo(
+                city=content['city'],
+                country=content['country'],
+                wind=content['win'],
+                wind_speed=content['win_speed'],
+                air_level=content['air_level'],
+                temperature_now=content['tem'],
+                temperature_lowest=content['tem2'],
+                temperature_highest=content['tem1'])
 
-        logging.info(f"response {wi}")
-        return wi
+            logging.info(f"response {wi}")
+            return wi
+        except Exception as e:
+            logging.error(e)
+            return "get_weather failed because of bad network"
 
     def _run(self, location: str) -> WeatherInfo:
         return GetWeatherInformationInChina.get_weather(location)
--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import initialize
 from agent.core import AgentBuilder
 from model.openai import get_openai_llm
 from agent.toolkits import GetWeatherInformationInChina
+
+import os
 if __name__ == '__main__':
     initialize.initialize()
-    llm = get_openai_llm(openai_api_key='')
+    print(f'your key is {os.environ["open_ai_key"]}')
+    llm = get_openai_llm(openai_api_key=os.environ['open_ai_key'])
     agent = AgentBuilder.build(llm=llm, tools=[GetWeatherInformationInChina()])
-    agent.run("我想知道现在澳洲堪培拉的天气如何")
+    agent.run("我想知道现在广州的天气如何")
 
 
 
