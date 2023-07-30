2b05975c52aed481c4dbbf2daaee75f67822b93f
yuhua6379 <yuhua6379@gmail.com>
2023-07-30 10:47:03 +0800

    feat: delete

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from model.openai import get_openai_llm
 from agent.toolkits import GetWeatherInformationInChina
 if __name__ == '__main__':
     initialize.initialize()
-    llm = get_openai_llm(openai_api_key='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')
+    llm = get_openai_llm(openai_api_key='')
     agent = AgentBuilder.build(llm=llm, tools=[GetWeatherInformationInChina()])
     agent.run("我想知道现在澳洲堪培拉的天气如何")
 
